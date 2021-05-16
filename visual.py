3739144c3b8d716019facc6c62e7c3ef36338367
Nijithkamal <nijithkamalcs15@gmail.com>
2021-05-16 17:22:09 +0530

    MyDemo

--- visual.py
+++ visual.py
@@ -4,37 +4,73 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib
+import time
 #matplotlib.use('Agg')
 import seaborn as sns
 #Remove Warnings
 st.balloons()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+# SIDE Bar
+st.sidebar.header("STREAMLIT APP")
+st.sidebar.subheader("Select your option")
+
+option = st.sidebar.selectbox( '',('Intro','Table', 'Pairplot', 'Heap','Lineplot'))
+
+# import dataset
+df = pd.read_csv('titanic.csv')
+# First ten rows
+tips = df.head(10)
+
+st.title("Welcome to the Demo Streamlit App")
 
 # SIDE Bar
-st.sidebar.header("Streamlit Assignment")
-st.sidebar.subheader("Nijithkamal")
+my_bar = st.progress(0)
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title("Tips_Dataset")
+if (my_bar.progress == 0):
+    st.write("wait..")
+else:
+    for percent_complete in range(100):
+        time.sleep(0.1)
+        my_bar.progress(percent_complete + 1)
+    st.write("completed loading..")
 
-#import dataset
-df = pd.read_csv('titanic.csv')
-#First thirty rows
-tips = df.head(10)
+#Disaplay Intro
+if ( option == 'Intro'):
+
+    st.markdown('In this app a data sheet is integrated')
+    st.markdown('** 3 charts ** have been drawn based on the same')
+    st.markdown('The data sheets is of  the passengers  travelled in ** Titanic **')
+    st.markdown('You can select the option from the side bar')
+
 #Display the table
-st.table(tips)
+elif (option == 'Table' ):
+    with st.beta_container():
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.title("Tips_Dataset")
+        st.table(tips)
+
 
 #pairplot
-st.subheader("Pairplot")
-sns.pairplot(tips,hue='Sex',palette='rainbow')
-st.pyplot()
+elif (option == 'Pairplot' ):
+
+    with st.beta_container():
+        st.subheader("Pairplot")
+        sns.pairplot(tips,hue='Sex',palette='rainbow')
+        st.pyplot()
 
 #Correation
-st.subheader("Heatmap")
-sns.heatmap(tips.corr(),cmap='coolwarm',annot=True)
-st.pyplot()
+elif (option == 'Heap'):
+
+    with st.beta_container():
+        st.subheader("Heatmap")
+        sns.heatmap(tips.corr(),cmap='coolwarm',annot=True)
+        st.pyplot()
 
 #lineplot
-st.subheader("lineplot")
-sns.set_theme(style="darkgrid")
-sns.lineplot(x="Survived", y="Age", hue="Sex", data=df)
-st.pyplot()
+elif (option == "Lineplot"):
+
+    with st.beta_container():
+        st.subheader("lineplot")
+        sns.set_theme(style="darkgrid")
+        sns.lineplot(x="Survived", y="Age", hue="Sex", data=df)
+        st.pyplot()
